cfd467eb7894415122aaaf1599746ad356dc75cc
krisfalk <krisfalk2007@hotmail.com>
2016-10-04 08:56:51 -0500

    initial upload

---
+++ EmployableApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmployableApp.MvcApplication" Language="C#" %>
---
+++ EmployableApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmployableApp.Startup))]
+namespace EmployableApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
